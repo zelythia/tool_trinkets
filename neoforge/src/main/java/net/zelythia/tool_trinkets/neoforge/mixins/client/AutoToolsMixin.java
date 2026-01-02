27fef2f8053013ef880fc92722913d2c31c7cbf9
lukas <dev@zelythia.net>
2026-01-02 15:59:59 +0100

    Publish Neoforge

--- neoforge/src/main/java/net/zelythia/tool_trinkets/neoforge/mixins/client/AutoToolsMixin.java
+++ neoforge/src/main/java/net/zelythia/tool_trinkets/neoforge/mixins/client/AutoToolsMixin.java
@@ -20,47 +20,43 @@ import java.util.Optional;
 @Mixin(AutoTools.class)
 public class AutoToolsMixin {
 
-
-    @Redirect(method = "getCorrectTool", at= @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;getContainerSize()I"))
+    @Redirect(method = "getCorrectTool", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;getContainerSize()I"))
     private static int onGetContainerSize(Inventory inventory) {
         int i = 0;
 
         Optional<ICuriosItemHandler> curiosItemHandler = CuriosApi.getCuriosInventory(Minecraft.getInstance().player);
-        if(curiosItemHandler.isPresent()) {
+        if (curiosItemHandler.isPresent()) {
             i = curiosItemHandler.get().getStacksHandler("tools").get().getStacks().getSlots();
         }
 
         return inventory.getContainerSize() + i;
     }
 
-    @Redirect(method = "getCorrectTool", at= @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;getItem(I)Lnet/minecraft/world/item/ItemStack;"))
+    @Redirect(method = "getCorrectTool", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;getItem(I)Lnet/minecraft/world/item/ItemStack;"))
     private static ItemStack onGetItem(Inventory inventory, int slot) {
-        if(slot >= inventory.getContainerSize()) {
+        if (slot >= inventory.getContainerSize()) {
             Optional<ICuriosItemHandler> curiosItemHandler = CuriosApi.getCuriosInventory(Minecraft.getInstance().player);
-            if(curiosItemHandler.isPresent()) {
+            if (curiosItemHandler.isPresent()) {
                 int size = inventory.getContainerSize();
                 return curiosItemHandler.get().getStacksHandler("tools").get().getStacks().getStackInSlot(slot - size);
             }
             return ItemStack.EMPTY;
-        }
-        else return inventory.getItem(slot);
+        } else return inventory.getItem(slot);
     }
 
 
     @Redirect(method = "selectItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handleInventoryMouseClick(IIILnet/minecraft/world/inventory/ClickType;Lnet/minecraft/world/entity/player/Player;)V"))
-    private static void onSelectItem(MultiPlayerGameMode instance, int id, int sourceSlot, int destSlot, ClickType clickType, Player player){
-        if(sourceSlot >= player.getInventory().getContainerSize()) {
+    private static void onSelectItem(MultiPlayerGameMode instance, int id, int sourceSlot, int destSlot, ClickType clickType, Player player) {
+        if (sourceSlot >= player.getInventory().getContainerSize()) {
             PacketDistributor.sendToServer(new ToolTrinketsNeoForge.MoveTrinketPayload(sourceSlot - player.getInventory().getContainerSize(), destSlot));
-        }
-        else instance.handleInventoryMouseClick(id, sourceSlot, destSlot, ClickType.SWAP, player);
+        } else instance.handleInventoryMouseClick(id, sourceSlot, destSlot, ClickType.SWAP, player);
     }
 
     @Redirect(method = "switchBack", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handleInventoryMouseClick(IIILnet/minecraft/world/inventory/ClickType;Lnet/minecraft/world/entity/player/Player;)V", ordinal = 1))
-    private static void onSwitchBack(MultiPlayerGameMode instance, int id, int sourceSlot, int destSlot, ClickType clickType, Player player){
-        if(sourceSlot >= player.getInventory().getContainerSize()) {
+    private static void onSwitchBack(MultiPlayerGameMode instance, int id, int sourceSlot, int destSlot, ClickType clickType, Player player) {
+        if (sourceSlot >= player.getInventory().getContainerSize()) {
             PacketDistributor.sendToServer(new ToolTrinketsNeoForge.MoveTrinketPayload(sourceSlot - player.getInventory().getContainerSize(), destSlot));
-        }
-        else instance.handleInventoryMouseClick(id, sourceSlot, destSlot, ClickType.SWAP, player);
+        } else instance.handleInventoryMouseClick(id, sourceSlot, destSlot, ClickType.SWAP, player);
     }
 }
 
